694ecf783f7315799957c0bb67c2aaac5f485d09
kornellewy <korneliuszlewczuk@gmail.com>
2020-05-13 15:41:08 +0200

    kjn

--- django_react/tweetme2/tweets/views.py
+++ django_react/tweetme2/tweets/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from .models import Tweet
 from django.shortcuts import get_object_or_404
+
+from .models import Tweet
+
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse('HUJ')
+    return render(request, "home.html")
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data= {
+        "isUser": False,
+        "response": tweets_list
+    }
+    return JsonResponse(data)
+    
 
 def tweet_detail_view(request, tweet_id):
     obj = get_object_or_404(Tweet, pk=tweet_id)
