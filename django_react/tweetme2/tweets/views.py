138c408f8caecd2bedf018ab19b044101551bfa0
kornellewy <korneliuszlewczuk@gmail.com>
2020-05-15 16:54:38 +0200

    kjn

--- django_react/tweetme2/tweets/views.py
+++ django_react/tweetme2/tweets/views.py
@@ -8,14 +8,29 @@ from .models import Tweet
 from .forms import TweetForm
 
 def tweet_create_view(request, *args, **kwargs):
+    user = request.user
+
+    if not request.user.is_authenticated:
+        user=None
+        if requ.is_ajax():
+            return JsonResponse({}, status=401)
+        return redirect(settings.LOGIN_URL)
+        
     form = TweetForm(request.POST or None)
     next_url = request.POST.get("next") or None
     if form.is_valid():
-        instance = form.save(commit=False)
-        instance.save()
+        obj = form.save(commit=False)
+        obj.user = user
+        obj.save()
+        if request.is_ajax():
+            return JsonResponse(obj.serialize(), status=201) # 201 to dla create items
+
         if next_url != None and is_safe_url(next_url, settings.ALLOWED_HOSTS):
             return redirect(next_url)
         form=TweetForm()
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors, status=400)
     return render(request, 'components/form.html', context={"form":form})
 
 def home_view(request, *args, **kwargs):
@@ -23,7 +38,7 @@ def home_view(request, *args, **kwargs):
 
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content, "likes": 1} for x in qs]
+    tweets_list = [x.serialize() for x in qs]
     data= {
         "isUser": False,
         "response": tweets_list
